9dfd10029c654bf81cac54b29c597e753206632c
alexblch <bloch.alexandr@gmail.com>
2025-06-10 12:10:33 +0200

    modif contact form

--- App/forms.py
+++ App/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label='Your Name')
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(widget=forms.Textarea, label='Your Message')
+    name = forms.CharField(max_length=100, label='Nom :')
+    email = forms.EmailField(label='Email :')
+    message = forms.CharField(widget=forms.Textarea, label='Message :')
